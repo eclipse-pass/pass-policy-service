170b763cd5a93e5d22bcb03e95a7b9640d9f9960
DeeMac <davidmcintyre@live.ie>
2022-11-10 21:48:02 +0000

    Added Initial Test Skeleton

--- src/test/java/org/eclipse/pass/policy/PassPolicyServiceTest.java
+++ src/test/java/org/eclipse/pass/policy/PassPolicyServiceTest.java
@@ -1,10 +1,18 @@
 package org.eclipse.pass.policy;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 /**
  * Unit Tests for the Policy Service
  *
  * @author David McIntyre
  */
 public class PassPolicyServiceTest {
+
+    @Test
+    public void demoTest() {
+        assertTrue(true);
+    }
 
 }
