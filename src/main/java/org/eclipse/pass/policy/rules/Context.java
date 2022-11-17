62ab1f8b134c273ff67705de3f01d9f6497bd375
DeeMac <davidmcintyre@live.ie>
2022-11-17 19:14:20 +0000

    Initial findPolicies() functionality

--- src/main/java/org/eclipse/pass/policy/rules/Context.java
+++ src/main/java/org/eclipse/pass/policy/rules/Context.java
@@ -1,20 +1,32 @@
 package org.eclipse.pass.policy.rules;
 
 import java.util.Map;
-//import org.dataconservancy.pass.client.PassClient;
-//import org.dataconservancy.pass.client.PassClientFactory;
+
+import org.dataconservancy.pass.client.PassClient;
+import org.eclipse.pass.policy.interfaces.VariablePinner;
 
 /**
  * Represents the Context object
  * Context establishes a rule evaluation/resolution context
- * 
+ *
  * @author David McIntyre
  */
-public class Context {
+public class Context implements VariablePinner {
 
     private String submissionURI;
     private Map headers;
-    // private PassEntityFetcher passClient;
+    private PassClient passClient;
     private Map values;
 
+    public Context(String submissionURI, Map headers, PassClient passClient) {
+        this.submissionURI = submissionURI;
+        this.headers = headers;
+        this.passClient = passClient;
+    }
+
+    @Override
+    public VariablePinner Pin(Variable variable, String string) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
--- src/main/java/org/eclipse/pass/policy/rules/DSL.java
+++ src/main/java/org/eclipse/pass/policy/rules/DSL.java
@@ -1,6 +1,8 @@
 package org.eclipse.pass.policy.rules;
 
 import org.dataconservancy.pass.model.Policy;
+import org.eclipse.pass.policy.interfaces.PolicyResolver;
+import org.eclipse.pass.policy.interfaces.VariablePinner;
 
 /**
  * Represents the DSL object
@@ -8,8 +10,16 @@ import org.dataconservancy.pass.model.Policy;
  *
  * @author David McIntyre
  */
-public class DSL {
+public class DSL implements PolicyResolver {
 
     private String schema; // json:"$schema"
     private Policy[] policies; // json:"policy-rules"
+
+    @Override
+    public Policy[] resolve(VariablePinner variables) {
+        Policy[] policies;
+
+        return null;
+    }
+
 }
