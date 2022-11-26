57e61170c5416226a697b0f40a90d0ce6ff1d348
DeeMac <davidmcintyre@live.ie>
2022-11-26 15:22:43 +0000

    resolve() Policies

--- src/main/java/org/eclipse/pass/policy/rules/Context.java
+++ src/main/java/org/eclipse/pass/policy/rules/Context.java
@@ -1,6 +1,7 @@
 package org.eclipse.pass.policy.rules;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -29,6 +30,13 @@ public class Context extends VariablePinner {
         this.values = new HashMap<String, Object>();
     }
 
+    public Context(String submissionURI, Map headers, PassClient passClient, Map<String, Object> values) {
+        this.submissionURI = submissionURI;
+        this.headers = headers;
+        this.passClient = passClient;
+        this.values = values;
+    }
+
     @Override
     public List<URI> resolve(URI varString) throws Exception {
         // resolve a variable returning a list of strings
@@ -36,6 +44,17 @@ public class Context extends VariablePinner {
         return null;
     }
 
+    /**
+     * pin()
+     *
+     * Pins a given value to a given variable.
+     * For pinning of context values, Objects must be of type URI.
+     *
+     * @param variable - the URI to pin to
+     * @param value    - the value to be pinned
+     * @return VariablePinner - the pinned object to be returned
+     * @throws Exception - incorrect object types supplied
+     */
     @Override
     public VariablePinner pin(Object variable, Object value) throws Exception {
         if (variable instanceof URI && value instanceof URI) {
@@ -48,15 +67,22 @@ public class Context extends VariablePinner {
             Map<String, Object> pinnedValues = new HashMap<String, Object>();
             Iterator it = headers.entrySet().iterator();
 
-            while(it.hasNext()) {
-                Map.Entry header = (Map.Entry)it.next();
-                headers = header.getValue();
+            while (it.hasNext()) {
+                Map.Entry header = (Map.Entry) it.next();
+                pinnedValues.put((String) header.getKey(), header.getValue());
             }
 
+            List<String> segments = Arrays.asList(parsed.getFullName().split("."));
 
-            }
-            return null;
+            pinnedValues.put(parsed.getFullName(), value);
+            pinnedValues.put(segments.get(segments.size() - 1), value);
+
+            this.values = pinnedValues;
+
+            return this;
+
         } else {
             throw new Exception("Must supply two URIs: A variable, and a value to pin");
         }
-    }}
+    }
+}
