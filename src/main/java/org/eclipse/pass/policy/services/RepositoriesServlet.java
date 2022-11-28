ad8bd5ad7fb7e1df8b3188f26e570959c0549bf9
DeeMac <davidmcintyre@live.ie>
2022-11-28 18:40:37 +0000

    resolve() Policies

--- src/main/java/org/eclipse/pass/policy/services/RepositoriesServlet.java
+++ src/main/java/org/eclipse/pass/policy/services/RepositoriesServlet.java
@@ -2,10 +2,9 @@ package org.eclipse.pass.policy.services;
 
 import java.io.IOException;
 import java.util.Enumeration;
-import java.util.List;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
