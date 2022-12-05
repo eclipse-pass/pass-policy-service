0da8a4a667e4c114f85d5a7fcfbeae97ce26d903
DeeMac <davidmcintyre@live.ie>
2022-12-05 16:04:38 +0000

    Initial Condition logic

--- src/main/java/org/eclipse/pass/policy/rules/Condition.java
+++ src/main/java/org/eclipse/pass/policy/rules/Condition.java
@@ -1,15 +1,66 @@
 package org.eclipse.pass.policy.rules;
 
-import java.util.Map;
+import java.util.List;
+
+import org.eclipse.pass.policy.interfaces.VariableResolver;
 
 /**
  * Represents the Condition object
- * Condition is a JSON object that determines whether a policy applies or not
- * 
+ * Condition determines whether a policy applies or not
+ *
  * @author David McIntyre
  */
 public class Condition {
 
-    private Map condition;
+    private List<String> conditions;
+
+    public Condition(List<String> conditions) {
+        this.conditions = conditions;
+    }
+
+    public Boolean apply(VariableResolver variables) throws Exception {
+        for (String cond : conditions) {
+            switch (cond) {
+                case "endsWith":
+                    endsWith(cond, variables);
+                    break;
+                case "equals":
+                    equals(cond, variables);
+                    break;
+                case "anyOf":
+                    anyOf(cond, variables);
+                    break;
+                case "noneOf":
+                    noneOf(cond, variables);
+                    break;
+                case "contains":
+                    contains(cond, variables);
+                    break;
+                default:
+                    throw new Exception("Unknown condition " + cond);
+            }
+        }
+        return null;
+    }
+
+    public Boolean endsWith(Object fromCondition, VariableResolver variables) {
+        return null;
+    }
+
+    public Boolean equals(Object fromCondition, VariableResolver variables) {
+        return null;
+    }
+
+    public Boolean anyOf(Object arg, VariableResolver variables) {
+        return null;
+    }
+
+    public Boolean noneOf(Object arg, VariableResolver variables) {
+        return null;
+    }
+
+    public Boolean contains(Object fromCondition, VariableResolver variables) {
+        return null;
+    }
 
 }
