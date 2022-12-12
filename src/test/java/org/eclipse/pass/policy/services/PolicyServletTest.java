646ceddcd5e691eef8269c826ac2fee6b192b71e
DeeMac <davidmcintyre@live.ie>
2022-12-12 12:56:34 +0000

    Unit tests added

--- src/test/java/org/eclipse/pass/policy/services/PolicyServletTest.java
+++ src/test/java/org/eclipse/pass/policy/services/PolicyServletTest.java
@@ -8,10 +8,18 @@ import static org.mockito.Mockito.when;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.pass.policy.services.PolicyServlet;
+import org.dataconservancy.pass.model.Policy;
+import org.eclipse.pass.policy.interfaces.PolicyService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,20 +35,37 @@ public class PolicyServletTest {
     @Test
     @DisplayName("Test: Test of doGet() method. Should return a response to a given request")
     void TestDoGet() throws Exception {
-        // mock HttpServletRequest & HttpServletResponse
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
+        // mock HttpServletRequest & HttpServletResponse & PolicyService
+        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+        HttpServletResponse mockResponse = mock(HttpServletResponse.class);
+        PolicyService mockPolicyService = mock(PolicyServiceImpl.class);
 
-        // mock the returned value of request.getParameter
-        when(request.getParameter("submission")).thenReturn("testSubmission");
+        // mock the returned values of
+        Map<String, String> headers = new HashMap();
+        headers.put("Context-Path", "test.submission.com");
+        headers.put("Query-String", "test.query");
+        Enumeration<String> headerNames = Collections.enumeration(headers.keySet());
+
+        when(mockRequest.getParameter("submission")).thenReturn("valid submission");
+        when(mockRequest.getHeaderNames()).thenReturn(headerNames);
+        when(mockRequest.getHeader("Context-Path")).thenReturn("test.submission.com");
+        when(mockRequest.getHeader("Query-String")).thenReturn("test.query");
+
+        // mock the returned value of policyService.findPolicies()
+        String submission = "valid submission";
+        List<Policy> policies = new ArrayList<Policy>();
+        policies.add(new Policy());
+        when(mockPolicyService.findPolicies(submission, headers)).thenReturn(policies);
 
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
-        when(response.getWriter()).thenReturn(writer);
+        when(mockResponse.getWriter()).thenReturn(writer);
 
-        new PolicyServlet().doGet(request, response);
+        new PolicyServlet(mockPolicyService).doGet(mockRequest, mockResponse);
 
-        verify(request, atLeast(1)).getParameter("submission");
+        verify(mockRequest, atLeast(1)).getParameter("submission");
+        verify(mockRequest, atLeast(1)).getHeaderNames();
+        verify(mockPolicyService, atLeast(1)).findPolicies(submission, headers);
         writer.flush();
         assertTrue(stringWriter.toString().contains("Served at:"));
     }
